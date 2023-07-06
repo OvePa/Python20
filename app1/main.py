5dc00e3ac73e89d869e7bdb187a4b4c709b7281f
OvePaz <omar.velazquez.paz@gmail.com>
2023-07-06 12:55:11 -0600

    app1 segundo dia

--- app1/main.py
+++ app1/main.py
@@ -1,17 +1,23 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
 
     match user_action.lower().strip():
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
+            x = 1
             for item in todos:
-                x = 1
                 print(x, item.title())
                 x = x + 1
+        case 'edit':
+            number = int(input('Number of the todo to edit: '))
+            number = number - 1
+            existing_todo = todos[number]
+            print('Todo to edit: ',existing_todo.title())
+            todos[number] = input('New Todo: ')
         case 'exit':
             break
         # case _:
