f4efc528cd13bd6e47b4d238b0bcd5333ad431ab
OvePaz <omar.velazquez.paz@gmail.com>
2023-07-13 12:42:41 -0600

    app1 tercer dia, se guarda archivo externo

--- app1/main.py
+++ app1/main.py
@@ -1,13 +1,23 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
 
     match user_action.lower().strip():
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + '\n'
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 print(f"{index + 1} -> {item.title()}")
 
