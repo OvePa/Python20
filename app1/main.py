ff0d2f97dc781d4f606c8e92de777d0c4445d11d
OvePaz <omar.velazquez.paz@gmail.com>
2023-07-24 19:39:35 -0600

    modules

--- app1/main.py
+++ app1/main.py
@@ -1,16 +1,4 @@
-def get_todos(filepath='todos.txt'):
-    """ Read the text file and return the list of todos. """
-    with open(filepath, 'r') as file:
-        todos_local = file.readlines()
-
-    return todos_local
-
-
-def write_todos(todos_arg, filepath='todos.txt'):
-    """ Write a to-to items list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+from functions import get_todos,write_todos
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ").lower().strip()
