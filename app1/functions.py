1c308d85193493f6aa276552bff6519c484d4b77
OvePaz <omar.velazquez.paz@gmail.com>
2023-07-26 13:00:11 -0600

    start GUI

--- app1/functions.py
+++ app1/functions.py
@@ -13,3 +13,7 @@ def write_todos(todos_arg, filepath=FILEPATH):
     """ Write a to-to items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+
+if __name__ == '__main__':
+    print("Hello")
