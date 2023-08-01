e7d0078877cd06df2f2693bd06ada17786dc6300
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-01 13:33:24 -0600

    txt to pdf done

--- Read txt to pdf/main.py
+++ Read txt to pdf/main.py
@@ -14,5 +14,5 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     pdf.cell(w=0, h=20, txt=filename.title(), ln=1)
     pdf.set_font(family='Times', size=15)
-    pdf.cell(w=0, h=15, txt=text)
+    pdf.multi_cell(w=0, h=10, txt=text)
     pdf.output(f"PDFFiles/{filename}.pdf")
