01ef9ecc1cfa4df433e48e5016d3ce9107448d4c
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-01 09:32:08 -0600

    Add more pages for each topic

--- PDF Creator/main.py
+++ PDF Creator/main.py
@@ -6,12 +6,15 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    pdf.add_page()
+    pages = row["Pages"]
+    for page in range(pages):
+        pdf.add_page()
 
-    pdf.set_font(family="Times", style="B", size=18)
-    pdf.set_text_color(153, 0, 0)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
-    pdf.line(10, 22, 200, 22)
+        pdf.set_font(family="Times", style="B", size=18)
+        pdf.set_text_color(153, 0, 0)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
+        pdf.cell(w=0, h=12, txt=f"{page + 1}/{pages}", align="R", ln=1)
+        pdf.line(10, 22, 200, 22)
 
 
 pdf.output("output.pdf")
