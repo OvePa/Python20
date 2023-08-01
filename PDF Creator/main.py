bd1e0cd85f5af39c0a1c93f559d2f93bb7d0bc1f
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-01 10:04:52 -0600

    Add lines

--- PDF Creator/main.py
+++ PDF Creator/main.py
@@ -16,10 +16,14 @@ for index, row in df.iterrows():
         pdf.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
         pdf.line(10, 22, 200, 22)
 
+        # Add lines in between
+        for y in range(32, 262, 10):
+            pdf.line(10, y, 200, y)
+
         # Footer
-        pdf.ln(240)
+        pdf.ln(242)
         pdf.set_font(family="Times", style="I", size=12)
-        pdf.line(10, 265, 200, 265)
+        pdf.line(10, 262, 200, 262)
         pdf.cell(w=170, h=12, txt="Footer", align="L")
         pdf.cell(w=20, h=12, txt=f"{page + 1}/{pages}", align="R", ln=1)
 
