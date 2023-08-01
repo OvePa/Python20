d208266b6b81c4f7c00bc002c1f863438f0a8462
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-01 12:11:50 -0600

    Read txt file to pdf

--- PDF Invoice Creator/main.py
+++ PDF Invoice Creator/main.py
@@ -7,11 +7,10 @@ filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    pdf = fpdf.FPDF(orientation='P',unit='mm',format='A4')
+    pdf = fpdf.FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
-    pdf.set_font(family='Times',size=16,style='B')
+    pdf.set_font(family='Times', size=16, style='B')
     filename = Path(filepath).stem
     invoice_nr = filename.split('-')[0]
-    pdf.cell(w=50,h=8,txt=f"Invoice nr. {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}")
     pdf.output(f'PDFs/{filename}.pdf')
-
---
+++ Read txt to pdf/main.py
@@ -0,0 +1,18 @@
+import glob
+import fpdf
+from pathlib import Path
+
+filepaths = glob.glob('TextFiles/*.txt')
+
+for filepath in filepaths:
+    with open(filepath, 'r') as f:
+        text = f.read()
+
+    pdf = fpdf.FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.set_font(family='Times', size=20, style='B')
+    filename = Path(filepath).stem
+    pdf.cell(w=0, h=20, txt=filename.title(), ln=1)
+    pdf.set_font(family='Times', size=15)
+    pdf.cell(w=0, h=15, txt=text)
+    pdf.output(f"PDFFiles/{filename}.pdf")
