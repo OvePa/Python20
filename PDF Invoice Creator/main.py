248e358cd6cacef6944913ebdf8b5b44a8f52a28
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-01 13:29:34 -0600

    Add header, total price, company and logo

--- PDF Invoice Creator/main.py
+++ PDF Invoice Creator/main.py
@@ -37,8 +37,28 @@ for filepath in filepaths:
     for index, row in df.iterrows():
         for col in range(len(cols)):
             name_col = cols[col]
-            pdf.set_font(family='Times', size=8, style='B')
+            pdf.set_font(family='Times', size=8)
             pdf.cell(w=38, h=9, txt=str(row[name_col]), align='L', border=1)
         pdf.cell(w=0, h=9, ln=1)
+    pdf.cell(w=0, h=0, ln=1)
+
+    # Total
+    total_invoice = df['total_price'].sum()
+    pdf.set_font(family='Times', size=8, style='B')
+    pdf.cell(w=114, h=9)
+    pdf.cell(w=38, h=9, txt='Total Sum', align='L', border=1)
+    pdf.cell(w=38, h=9, txt=f"$ {total_invoice}", align='L', border=1,ln=1)
+
+    pdf.cell(w=0, h=9, ln=3)
+
+    # Total sentence
+    pdf.set_font(family='Times', size=14, style='B')
+    pdf.cell(w=0, h=9, txt=f"Total of the price is $ {total_invoice}", align="L", ln=1)
+
+    # Logo
+    pdf.set_font(family='Times', size=20, style='B')
+    pdf.cell(w=30, h=9, txt="Python", align='L')
+    pdf.image('pythonhow.png',w=10)
+
 
     pdf.output(f'PDFs/{filename}.pdf')
