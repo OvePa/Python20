ac6e74fabfcba045296c130377644f50e8ba7f54
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-01 13:09:42 -0600

    Add a table

--- PDF Invoice Creator/main.py
+++ PDF Invoice Creator/main.py
@@ -6,25 +6,39 @@ from pathlib import Path
 filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
     pdf = fpdf.FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
 
     # Get the name of the file
     filename = Path(filepath).stem
-    invoice_nr,date = filename.split('-')
+    invoice_nr, date = filename.split('-')
 
     # Puts the title of the invoice
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=0, h=8, txt=f"Invoice nr. {invoice_nr}",ln=1)
+    pdf.cell(w=0, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
 
     # Create the date
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=0, h=8, txt=f"Date: {date}")
+    pdf.cell(w=0, h=8, txt=f"Date: {date}", ln=3)
+    pdf.cell(w=0, h=12, ln=2)
 
-    
+    # Add the column names
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
 
+    cols = df.columns
+    cols_names = [item.replace("_", ' ').title() for item in cols]
+    for col in range(len(cols_names)):
+        pdf.set_font(family='Times', size=12, style='B')
+        pdf.cell(w=38, h=12, txt=cols_names[col], align='C', border=1)
+    pdf.cell(w=0, h=12, ln=1)
 
-
+    # Add rows
+    for index, row in df.iterrows():
+        for col in range(len(cols)):
+            name_col = cols[col]
+            pdf.set_font(family='Times', size=8, style='B')
+            pdf.cell(w=38, h=9, txt=str(row[name_col]), align='L', border=1)
+        pdf.cell(w=0, h=9, ln=1)
 
     pdf.output(f'PDFs/{filename}.pdf')
