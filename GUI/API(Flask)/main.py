f243e1ab30512740d8e9b76b66f12540253bb4de
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-03 11:54:13 -0600

    Add the dictionary file

--- GUI/API(Flask)/main.py
+++ GUI/API(Flask)/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv("dictionary.csv")
 
 
 @app.route("/")
@@ -10,7 +13,8 @@ def home():
 
 @app.route("/api/v1/<word>")
 def word_def(word):
-    definition = word.upper()
+    word = word.lower()
+    definition = df[df["word"] == word]["definition"].squeeze()
     return {"definition": definition, "word": word}
 
 
