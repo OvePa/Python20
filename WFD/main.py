53aef35aa56ef17a9a027e04ed11b4dd4e21e6cc
OvePaz <omar.velazquez.paz@gmail.com>
2023-08-04 13:19:21 -0600

    Add imgs and fix errors

--- WFD/main.py
+++ WFD/main.py
@@ -2,29 +2,43 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Add title, text input, slider, selectbox and subheader
 st.title("Weather Forcast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider(
     "Forcast Days:",
     min_value=1,
     max_value=5,
-    help="Select the number of forcasted days",
+    help="Select the number of forecasted days",
 )
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-get_data(place, days, option)
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place, days)
+    if filtered_data != "Wrong City":
+        if option == "Temperature":
+            temperature = [
+                str(int(dict["main"]["temp"]) / 10) for dict in filtered_data
+            ]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(
+                x=dates, y=temperature, labels={"x": "Date", "y": "Temperatures (c)"}
+            )
+            st.plotly_chart(figure)
 
+        if option == "Sky":
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png",
+            }
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
 
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-
-d, t = get_data(days)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperatures (c)"})
-
-st.plotly_chart(figure)
+    else:
+        st.warning("Wrong City")
